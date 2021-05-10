608a1b47d5145205195691666d5fae278f948867
Curtis Fulton <Curtis.R.Fulton@Gmail.com>
2021-05-10 19:55:31 +1000

    Setup SvelteKit

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a class="text-blue-500 underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style global lang="postcss">
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
